fe8aa6466ad3ee5ecb6a79193f4cefa19e4567a2
MakotoFukase <dnfg.e.m.f@gmail.com>
2020-02-19 23:26:53 +0900

    【フロント_詳細画面】画面作成

--- app/Http/Controllers/test_Fukase/DetailController.php
+++ app/Http/Controllers/test_Fukase/DetailController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\test_Fukase;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Model\Site;
 
 class DetailController extends Controller
 {
-    //
+    // 一覧画面
+    public function detail(Request $request) 
+    {
+        $items = DB::select('select * from site where id=1');
+        \Log::info('ログ出力テスト');
+        logger($items);
+        return view('fukase_test.detail', ['items'=>$items]);
+    }
 }
---
+++ resources/views/fukase_test/detail.blade.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <link rel="shortcut icon" href="../../../images/favicon.ico"/>  
+    <title>DBサイト</title>
+    <style>
+    body { font-size:16pt; color:#555; }
+    h1 { font-size:100pt; text-align:right; color:#f6f6f6;
+        margin:-50px 0px -100px 0px; }
+    th {background-color:#999; color:fff; padding:5px 10px; }
+    td {border: solid 1px #aaa; color:#999; padding:5px 10px; }
+    </style>
+</head>
+<body>
+    <h1>Detail</h1>
+    <p>詳細</p>
+
+    <table>
+    <tr>
+        <th>image</th>
+        <th>name</th>
+        <th>attribute1</th>
+        <th>attribute2</th>
+        <th>attribute3</th>
+        <th>attribute4</th>
+        <th>attribute5</th>
+    </tr>
+
+    <!-- 繰り返し -->
+    @foreach ($items as $item)
+        <tr>
+            <td>{{$item->main_path}}</td>
+            <td>{{$item->site_name}}</td>
+            <td>{{$item->at1_name}}</td>
+            <td>{{$item->at2_name}}</td>
+            <td>{{$item->at3_name}}</td>
+            <td>{{$item->at4_name}}</td>
+            <td>{{$item->at5_name}}</td>
+        </tr>
+    @endforeach
+    </table><br>
+</body>
+</html>
